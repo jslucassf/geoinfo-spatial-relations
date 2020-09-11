54f1f6139a2e46844310e415e1907efff8665e90
jslucassf <jslucassf@gmail.com>
2020-09-11 10:15:37 -0300

    Finishes first version of paper

--- fixLinestrings.py
+++
@@ -1,8 +0,0 @@
-import json
-import pandas as pd
-
-drawings = pd.read_csv("data/drawings.csv", sep=',', quotechar='"')
-
-for ind, row in drawings.iterrows():
-    geom = json.loads(row['geometry'])
-    print(geom)
